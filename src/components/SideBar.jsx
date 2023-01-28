73f5365bbcbea593b533b0e86c8cf6d568f5163f
Jiashu Li <452472803ljs@gmail.com>
2023-01-28 00:19:28 -0500

    Finished DataFetch and videos are fetched from youtubeAPI

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,7 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const selectedCategory = "New";
-const SideBar = () => (
+const SideBar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -14,6 +13,7 @@ const SideBar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#Fc1503",
           color: "white",
